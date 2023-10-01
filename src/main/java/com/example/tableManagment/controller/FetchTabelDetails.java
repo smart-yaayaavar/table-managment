91b938cbdfacbd0ec98c3abfdee5ad0e084bc5b6
smart yaayaavar <smart.yaayaavar@gmail.com>
2023-10-01 13:17:35 +0530

    first commit

---
+++ src/main/java/com/example/tableManagment/controller/FetchTabelDetails.java
@@ -0,0 +1,18 @@
+package com.example.tableManagment.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController()
+@RequestMapping("/v1")
+public class FetchTabelDetails {
+
+    @GetMapping("/table/details")
+    public String getTableDetails(){
+        return "Hi";
+    }
+}
